aaae108f22e4b8962eb3085de243ae8d27147e2f
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-06-15 22:42:24 +0530

    Code For Calculating Fare

--- src/main/java/com/magic/cabinvoicegenerator/CabInvoiceGenerator.java
+++ src/main/java/com/magic/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -1,5 +1,14 @@
 package com.magic.cabinvoicegenerator;
 
 public class CabInvoiceGenerator {
-  
+    public static final int rate = 10;
+    public static final int minimumFare = 5;
+    public double calculateFare(double distance, int time){
+        double totalFare = distance*rate+time;
+        if (totalFare < 5)
+            return 5;
+        else{
+            return totalFare;
+        }
+    }
 }
